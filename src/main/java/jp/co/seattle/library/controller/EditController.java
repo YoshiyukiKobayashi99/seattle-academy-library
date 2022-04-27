aff56d38031b03e89f8a2d470968fc15dbb9d62a
YoshiyukiKobayashi99 <yoshiyuki.kobayashi@seattleconsulting.co.jp>
2022-04-27 17:37:04 +0900

    タスク7機能実装

---
+++ src/main/java/jp/co/seattle/library/controller/EditController.java
@@ -0,0 +1,144 @@
+package jp.co.seattle.library.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import jp.co.seattle.library.dto.BookDetailsInfo;
+import jp.co.seattle.library.service.BooksService;
+import jp.co.seattle.library.service.ThumbnailService;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller // APIの入り口
+public class EditController {
+	final static Logger logger = LoggerFactory.getLogger(EditController.class);
+
+	@Autowired
+	private BooksService booksService;
+
+	@Autowired
+	private ThumbnailService thumbnailService;
+
+	/**
+	 * 詳細画面に遷移する
+	 * 
+	 * @param locale
+	 * @param bookId
+	 * @param model
+	 * @return
+	 */
+	@Transactional
+	@RequestMapping(value = "/editBook", method = RequestMethod.POST) // value＝actionで指定したパラメータ
+	public String editBook(Locale locale, @RequestParam("bookId") Integer bookId, Model model) {
+
+		logger.info("Welcome editControler.java! The client locale is {}.", locale);
+
+		model.addAttribute("bookDetailsInfo", booksService.getBookInfo(bookId));
+
+		return "edit";
+	}
+
+	/**
+	 * 書籍情報を登録する
+	 * 
+	 * @param locale       ロケール情報
+	 * @param title        書籍名
+	 * @param author       著者名
+	 * @param publisher    出版社
+	 * @param file         サムネイルファイル
+	 * @param model        モデル
+	 * @param publish_date 出版日
+	 * @param isbn         ISBN
+	 * @param description  説明文
+	 * @return 遷移先画面
+	 */
+	@Transactional
+	@RequestMapping(value = "/editUpdateBook", method = RequestMethod.POST, produces = "text/plain;charset=utf-8")
+	public String editUpdateBookBook(Locale locale, @RequestParam("title") String title,
+			@RequestParam("author") String author, @RequestParam("publisher") String publisher,
+			@RequestParam("thumbnail") MultipartFile file, @RequestParam("publish_date") String publishDate,
+			@RequestParam("isbn") String isbn, @RequestParam("description") String description,
+			@RequestParam("bookId") Integer bookId,
+
+			Model model) {
+		logger.info("Welcome insertBooks.java! The client locale is {}.", locale);
+
+		// パラメータで受け取った書籍情報をDtoに格納する。
+		BookDetailsInfo bookInfo = new BookDetailsInfo();
+		bookInfo.setBookId(bookId);
+		bookInfo.setTitle(title);
+		bookInfo.setAuthor(author);
+		bookInfo.setPublisher(publisher);
+		bookInfo.setDescription(description);
+		bookInfo.setIsbn(isbn);
+		bookInfo.setPublishDate(publishDate);
+
+		List<String> errorMessage = new ArrayList<>();
+
+		// クライアントのファイルシステムにある元のファイル名を設定する
+		String thumbnail = file.getOriginalFilename();
+
+		if (!file.isEmpty()) {
+			try {
+				// サムネイル画像をアップロード
+				String fileName = thumbnailService.uploadThumbnail(thumbnail, file);
+				// URLを取得
+				String thumbnailUrl = thumbnailService.getURL(fileName);
+
+				bookInfo.setThumbnailName(fileName);
+				bookInfo.setThumbnailUrl(thumbnailUrl);
+
+			} catch (Exception e) {
+
+				// 異常終了時の処理
+				logger.error("サムネイルアップロードでエラー発生", e);
+				model.addAttribute("bookDetailsInfo", bookInfo);
+				return "addBook";
+			}
+		}
+
+		if ((title.isEmpty()) || (author.isEmpty()) || (publisher.isEmpty()) || (publishDate.isEmpty())) {
+			errorMessage.add("必須項目を入力してください。<p><br>");
+		}
+
+		if (!(publishDate.matches("^[0-9]{8}$"))) {
+			errorMessage.add("出版日は半角数字はYYYYMMDD形式で入力してください。<p><br>");
+		}
+
+		if ((!isbn.isEmpty()) && !((isbn.matches("^[0-9]{10}$"))) && (!(isbn.matches("^[0-9]{13}$")))) {
+			errorMessage.add("ISBNは10桁または13桁の半角数字が正しくありません。");
+		}
+
+		if (errorMessage == null || errorMessage.size() == 0) {
+			booksService.updateBook(bookInfo, bookId);
+
+			model.addAttribute("resultMessage", "更新完了");
+
+			model.addAttribute("bookDetailsInfo", booksService.getBookInfo(bookId));
+
+			// TODO 登録した書籍の詳細情報を表示するように実装
+			// 詳細画面に遷移する
+			return "details";
+
+		} else {
+			model.addAttribute("errorMessage", errorMessage);
+			model.addAttribute("bookDetailsInfo", bookInfo);
+			return "addBook";
+		}
+
+	}
+
+}
--- src/main/java/jp/co/seattle/library/service/BooksService.java
+++ src/main/java/jp/co/seattle/library/service/BooksService.java
@@ -16,94 +16,106 @@ import jp.co.seattle.library.rowMapper.BookInfoRowMapper;
 /**
  * 書籍サービス
  * 
- *  booksテーブルに関する処理を実装する
+ * booksテーブルに関する処理を実装する
  */
 @Service
 public class BooksService {
-    final static Logger logger = LoggerFactory.getLogger(BooksService.class);
-    @Autowired
-    private JdbcTemplate jdbcTemplate;
+	final static Logger logger = LoggerFactory.getLogger(BooksService.class);
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
 
-    /**
-     * 書籍リストを取得する
-     *
-     * @return 書籍リスト
-     */
-    public List<BookInfo> getBookList() {
-        // TODO 取得したい情報を取得するようにSQLを修正
-        List<BookInfo> getedBookList = jdbcTemplate.query(
-        		//タイトル順に
-                "select id, title, author, publisher, publish_date, thumbnail_url from books order by title",
-                new BookInfoRowMapper());
+	/**
+	 * 書籍リストを取得する
+	 *
+	 * @return 書籍リスト
+	 */
+	public List<BookInfo> getBookList() {
+		// TODO 取得したい情報を取得するようにSQLを修正
+		List<BookInfo> getedBookList = jdbcTemplate.query(
+				// タイトル順に
+				"select id, title, author, publisher, publish_date, thumbnail_url from books order by title",
+				new BookInfoRowMapper());
 
-        return getedBookList;
-    }
+		return getedBookList;
+	}
 
-    /**
-     * 書籍IDに紐づく書籍詳細情報を取得する
-     *
-     * @param bookId 書籍ID
-     * @return 書籍情報
-     */
-    public BookDetailsInfo getBookInfo(int bookId) {
+	/**
+	 * 書籍IDに紐づく書籍詳細情報を取得する
+	 *
+	 * @param bookId 書籍ID
+	 * @return 書籍情報
+	 */
+	public BookDetailsInfo getBookInfo(int bookId) {
 
-        // JSPに渡すデータを設定する
-        String sql = "SELECT * FROM books where id ="
-                + bookId;
+		// JSPに渡すデータを設定する
+		String sql = "SELECT * FROM books where id =" + bookId;
 
-        BookDetailsInfo bookDetailsInfo = jdbcTemplate.queryForObject(sql, new BookDetailsInfoRowMapper());
+		BookDetailsInfo bookDetailsInfo = jdbcTemplate.queryForObject(sql, new BookDetailsInfoRowMapper());
 
-        return bookDetailsInfo;
-    }
-    
-    /**
-     * 
-     * 書籍IDに紐づく書籍詳細情報を引数なしで取得する
-     * 
-     * @param bookId 書籍ID
-     * @return 書籍情報
-     */
-    
-    public BookDetailsInfo getBookInfo() {
+		return bookDetailsInfo;
+	}
 
-        String sql = "select * from books where id = (select max(id) from books);";
+	/**
+	 * 
+	 * 書籍IDに紐づく書籍詳細情報を引数なしで取得する
+	 * 
+	 * @param bookId 書籍ID
+	 * @return 書籍情報
+	 */
 
-        BookDetailsInfo bookDetailsInfo = jdbcTemplate.queryForObject(sql, new BookDetailsInfoRowMapper());
+	public BookDetailsInfo getBookInfo() {
 
-        return bookDetailsInfo;
-    }
+		String sql = "select * from books where id = (select max(id) from books);";
 
-    /**
-     * 書籍を登録する
-     *
-     * @param bookInfo 書籍情報
-     */
-    
-    public void registBook(BookDetailsInfo bookInfo) {
+		BookDetailsInfo bookDetailsInfo = jdbcTemplate.queryForObject(sql, new BookDetailsInfoRowMapper());
 
-        String sql = "INSERT INTO books (title, author, publisher, thumbnail_name, thumbnail_url, publish_date, isbn, description, reg_date, upd_date) VALUES ('"
-                + bookInfo.getTitle() + "','" + bookInfo.getAuthor() + "','" + bookInfo.getPublisher() + "','"
-                + bookInfo.getThumbnailName() + "','"
-                + bookInfo.getThumbnailUrl() + "','"
-                + bookInfo.getPublishDate() + "','"
-                + bookInfo.getIsbn() + "','"
-                + bookInfo.getDescription() + "',"
-                + "now(),"
-                + "now())";
+		return bookDetailsInfo;
+	}
 
-        jdbcTemplate.update(sql);
-    }
-    
-    /**
-     * 書籍を削除する
-     * 
-     * @param bookId 書籍ID
-     */
-    
-    public void deleteBook(int bookId) {
-    	
-    	String sql = "DELETE FROM books WHERE id = '" + bookId + "'";
-    	
-    	jdbcTemplate.update(sql);
-    }
+	/**
+	 * 書籍を登録する
+	 *
+	 * @param bookInfo 書籍情報
+	 */
+
+	public void registBook(BookDetailsInfo bookInfo) {
+
+		String sql = "INSERT INTO books (title, author, publisher, thumbnail_name, thumbnail_url, publish_date, isbn, description, reg_date, upd_date) VALUES ('"
+				+ bookInfo.getTitle() + "','" + bookInfo.getAuthor() + "','" + bookInfo.getPublisher() + "','"
+				+ bookInfo.getThumbnailName() + "','" + bookInfo.getThumbnailUrl() + "','" + bookInfo.getPublishDate()
+				+ "','" + bookInfo.getIsbn() + "','" + bookInfo.getDescription() + "'," + "now()," + "now())";
+
+		jdbcTemplate.update(sql);
+	}
+
+	/**
+	 * 書籍を更新する
+	 *
+	 * @param bookInfo 書籍情報
+	 * @param bookId   書籍ID
+	 */
+
+	public void updateBook(BookDetailsInfo bookInfo, int id) {
+
+		String sql = "UPDATE books SET (title, author, publisher, thumbnail_name, thumbnail_url, publish_date, isbn, description, upd_date) = ('"
+				+ bookInfo.getTitle() + "','" + bookInfo.getAuthor() + "','" + bookInfo.getPublisher() + "','"
+				+ bookInfo.getThumbnailName() + "','" + bookInfo.getThumbnailUrl() + "','" + bookInfo.getPublishDate()
+				+ "','" + bookInfo.getIsbn() + "','" + bookInfo.getDescription() + "'," + "now())" + "WHERE id = " + id
+				+ "";
+
+		jdbcTemplate.update(sql);
+	}
+
+	/**
+	 * 書籍を削除する
+	 * 
+	 * @param bookId 書籍ID
+	 */
+
+	public void deleteBook(int bookId) {
+
+		String sql = "DELETE FROM books WHERE id = '" + bookId + "'";
+
+		jdbcTemplate.update(sql);
+	}
 }
